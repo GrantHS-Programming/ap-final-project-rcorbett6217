492b71b768860e6f520f7248174e47bf7e898b4c
rcorbett6217 <asd@>
2024-05-09 10:00:21 -0700

    commit

---
+++ Gomoku.java
@@ -0,0 +1,86 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+public class Gomoku implements ActionListener {
+    JFrame window = new JFrame("Gomoku Board");
+    JButton[][] board = new JButton[15][15];
+    int[][] boardMatrix = new int[15][15];
+
+    private boolean currentPlayer = true;
+
+    public static void main(String[] args) {
+        new Gomoku();
+    }
+
+    public Gomoku(){
+        window.setLayout(new BorderLayout());
+        window.setSize(800, 800);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container boardContainer = new Container();
+        boardContainer.setLayout(new GridLayout(15,15));
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                board[row][col] = new JButton();
+                board[row][col].addActionListener(this);
+                boardContainer.add(board[row][col]);
+            }
+
+        }
+
+        window.add(boardContainer, BorderLayout.CENTER);
+        window.setVisible(true);
+    }
+    public void checkWin(int player){
+        for (int row = 0; row < boardMatrix.length; row++) {
+            for (int col = 0; col < boardMatrix[0].length; col++) {
+                if(boardMatrix[row][col] == player){
+                    //check right
+                    if(col <= 10) {
+                        if (boardMatrix[row][col + 1] == player && boardMatrix[row][col + 2] == player && boardMatrix[row][col + 3] == player && boardMatrix[row][col + 4] == player) {
+                            System.out.println("win");
+                        }
+                    }
+                    //check down
+                    if(row <= 10){
+                        if (boardMatrix[row + 1][col] == player && boardMatrix[row + 2][col] == player && boardMatrix[row + 3][col] == player && boardMatrix[row + 4][col] == player) {
+                            System.out.println("win");
+                        }
+                    }
+                    //check down and to the right
+                    if(col <= 10 && row <= 10){
+                        if (boardMatrix[row + 1][col + 1] == player && boardMatrix[row + 2][col + 2] == player && boardMatrix[row + 3][col + 3] == player && boardMatrix[row + 4][col + 4] == player) {
+                            System.out.println("win");
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if(e.getSource().equals(board[row][col])){
+                    board[row][col].setEnabled(false);
+                    if(currentPlayer){
+                        board[row][col].setText("X");
+                        boardMatrix[row][col] = 1;
+                        checkWin(1);
+                        currentPlayer = false;
+                    }
+                    else{
+                        board[row][col].setText("O");
+                        boardMatrix[row][col] = 2;
+                        checkWin(2);
+                        currentPlayer = true;
+                    }
+                }
+            }
+        }
+
+    }
+}
